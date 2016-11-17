de8df0a09974a394c65284f35f1e673fae4ee602
Ken Hebel <khebel@enova.com>
2016-11-17 14:14:31 -0600

    gofmt tabssss

--- exercise-001-corpus/word_count.go
+++ exercise-001-corpus/word_count.go
@@ -1,52 +1,51 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "strings"
-  "sort"
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
 )
 
-
 func main() {
-  // Read in file
-  data, err := ioutil.ReadFile("7oldsamr.txt")
-  if err != nil {
-      panic(err)
-  }
+	// Read in file
+	data, err := ioutil.ReadFile("7oldsamr.txt")
+	if err != nil {
+		panic(err)
+	}
 
-  // filter out punctuation
-  replacer := strings.NewReplacer(",", "", ".", "", ";", "", "'", "", "!", "", "?", "", ":", "", "\"", "")
-  parsedData := replacer.Replace(string(data))
+	// filter out punctuation
+	replacer := strings.NewReplacer(",", "", ".", "", ";", "", "'", "", "!", "", "?", "", ":", "", "\"", "")
+	parsedData := replacer.Replace(string(data))
 
-  // split data from file by word
-  words := strings.Fields(string(parsedData))
+	// split data from file by word
+	words := strings.Fields(string(parsedData))
 
-  // count all words as lowercase
-  dict := map[string]int{}
-  for _, word := range words {
-    dict[strings.ToLower(word)] += 1
-  }
+	// count all words as lowercase
+	dict := map[string]int{}
+	for _, word := range words {
+		dict[strings.ToLower(word)] += 1
+	}
 
-  // reverse keys and values { 10: ["face", "cool", "gg"], 4: ["mm"], ... }
-  reverseDict := map[int][]string{}
-  counts := []int{}
-  for word, count := range dict {
-    reverseDict[count] = append(reverseDict[count], word)
-  }
+	// reverse keys and values { 10: ["face", "cool", "gg"], 4: ["mm"], ... }
+	reverseDict := map[int][]string{}
+	counts := []int{}
+	for word, count := range dict {
+		reverseDict[count] = append(reverseDict[count], word)
+	}
 
-  // Grab all of the keys from the reversed dict to sort
-  for count := range reverseDict {
-    counts = append(counts, count)
-  }
+	// Grab all of the keys from the reversed dict to sort
+	for count := range reverseDict {
+		counts = append(counts, count)
+	}
 
-  // Sort
-  sort.Sort(sort.Reverse(sort.IntSlice(counts)))
+	// Sort
+	sort.Sort(sort.Reverse(sort.IntSlice(counts)))
 
-  // Output
-  for _, count := range counts {
-    for _, word := range reverseDict[count] {
-      fmt.Printf("%s %d\n", word, count)
-    }
-  }
+	// Output
+	for _, count := range counts {
+		for _, word := range reverseDict[count] {
+			fmt.Printf("%s %d\n", word, count)
+		}
+	}
 }
